4c996a5d9109062e48c744d93cead8156d85294b
npekhlivanov <nicko@omegasoft.bg>
2018-08-07 01:26:25 +0300

    Added SignalR test app

---
+++ AspNetCore/SignalRChat/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task GetNotification(string user)
+        {
+            await Clients.Caller.SendAsync("ReceiveNotification", string.Format("notification for user {0}: {1}", user, DateTime.Now));
+        }
+    }
+}
